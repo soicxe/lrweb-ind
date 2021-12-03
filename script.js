2d452a7a9a04701fc0cc8b4864730b3763801a79
soicxe <75142505+soicxe@users.noreply.github.com>
2021-12-03 22:48:45 +0300

    d

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿$("#lr07").click(function () {
+    window.open('index7.html');
+});
+$("#lr08").click(function () {
+    window.open('index8.html');
+});
+$("#lr09").click(function () {
+    window.open('index9.html');
+});
+$("#lr10").click(function () {
+    window.open('index10.html');
+});
---
+++ script7.js
@@ -0,0 +1,39 @@
+﻿function isEmpty(str) {
+	if (str.trim() == '') {
+		return true;
+	}
+	return false;
+}
+function anim() {
+	document.getElementById("circle").animate(
+		[
+			{ transform: "translateY(0) translateX(0)" },
+			{ transform: "translateY(-50px) translateX(50px)" }
+		],
+		{
+			easing: "cubic-bezier(0.445, 0.050, 0.550, 0.950)",
+			duration: 150,
+			direction: "alternate",
+			iterations: 4
+		}
+	)
+}
+function answer() {
+	if (isEmpty(document.getElementById("question").value)) {
+		alert("To get an answer enter a question");
+		return;
+	}
+	anim();
+	let rand = Math.floor(Math.random() * 4);
+	switch (rand) {
+		case 0:
+			document.getElementById("pp").textContent = "no";
+			break;
+		case 1:
+			document.getElementById("pp").textContent = "yes";
+			break;
+		case 2:
+			document.getElementById("pp").textContent = "maybe";
+			break;
+	}
+}
